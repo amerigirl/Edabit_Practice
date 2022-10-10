81da70b9a80451fc2552213affc047cb60f42b55
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-10-09 19:14:16 -0500

    finished method and call for TriangleWork

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,18 @@ package com.company;
 import java.util.Arrays;
 
 import static com.company.Multiples.arrayOfMultiples;
+import static com.company.TriangleWork.sideLengths;
 
 public class Main {
 
     public static void main(String[] args) {
         //call to arrayofMultiples method
-        System.out.println(Arrays.toString(arrayOfMultiples(4, 3))); //in order to print the call---wrap in Arrays.toString
+        System.out.println("Array Multiples: " + Arrays.toString(arrayOfMultiples(4, 3))); //in order to print the call---wrap in Arrays.toString
+
+
+        //call to TriangleWork method
+        System.out.println("Triangle Work (30-60-90): " + Arrays.toString(sideLengths(8)));
+
     }
 
 }
---
+++ src/com/company/TriangleWork.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class TriangleWork {
+    public static double[] sideLengths(int lineLength) {
+
+        /* Given the shortest side of a 30° by 60° by 90° triangle, find out the other two sides. Return the longest side and medium-length side in that order. *Double its(the shortest side) length to find the hypotenuse. You can multiply the short side by the square root of 3 to find the long leg.
+
+    1. create the class and method call
+    2. hypotenuse = n * 2; (find first maybe, but return second)
+    3. long leg = n * (square root 3); (find last but return first) */
+
+
+        double hypotenuse = lineLength * 2;
+        double longestLeg = lineLength * Math.sqrt(3);
+
+        double otherSide1 = longestLeg;
+        double otherSide2 = hypotenuse;
+
+        return new double[] {otherSide2, otherSide1};
+    }
+}
