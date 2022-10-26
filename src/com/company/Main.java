be2d46fe09f28e6c5d23338130bd6047cd537024
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-10-26 13:09:16 -0500

    finishing the equal or higher than method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import static com.company.easy.CountPoints.footballPoints;
 import static com.company.easy.EasyAdd.addition;
+import static com.company.easy.IsEqual.isHigherOrEqual;
 import static com.company.easy.IsSame.isSame;
 import static com.company.medium.Fibonacci.fibonacciFormula;
 import static com.company.medium.Multiples.arrayOfMultiples;
@@ -36,6 +37,9 @@ public class Main {
         System.out.println("Choose a index number and we will tell you what number is there! " + fibonacciFormula(7));
 
         System.out.println("Are these two numbers the same? " + isSame(3,3));
+
+        int[] anotherArray = new int[20];
+        System.out.println("Is there a number equal to or larger than 22? " + isHigherOrEqual(anotherArray, 22));
 
     }
 
