f2773e8a2f00a56416e8243fff253e2cf5edb5f7
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-09-12 23:01:07 -0500

    reviewed CanBeNested

--- src/com/company/easy/CanBeNested.java
+++ src/com/company/easy/CanBeNested.java
@@ -3,17 +3,25 @@ package com.company.easy;
 import java.util.Arrays;
 
 public class CanBeNested {
+    /*Create a function that returns true if the first array can be nested inside the second and false otherwise. firstArray can be nested inside secondArray if:
+
+          firstArray's min is greater than secondArray's min.
+          [3,5,7,8]
+
+          firstArray's max is less than secondArray's max.
+          [0,3,5,7,9]
+
+          step 1: create the method with the parameters
+          step 2 create a boolean
+          step 3. sort each array
+          4. get the length of each array and put it in a param
+          5. create a for loop the cycle the length of the second array
+          6. create a conditional that gets the length of each array  at the proper indexes and compares them
+
+      * */
+
     public static boolean canNest(int[] firstArray, int[] secondArray){
-        /*Create a function that returns true if the first array can be nested inside the second and false otherwise. firstArray can be nested inside secondArray if:
-
-            firstArray's min is greater than secondArray's min.
-            [3,5,7,8]
-
-            firstArray's max is less than secondArray's max.
-            [0,3,5,7,9]
-        * */
-
-        boolean nestable = true;
+        boolean canNest = true;
 
         Arrays.sort(firstArray);
         Arrays.sort(secondArray);
@@ -21,14 +29,15 @@ public class CanBeNested {
         int firstLength = firstArray.length;
         int secondLength = secondArray.length;
 
-        for(int i = 0; i < secondArray.length; i++) {
-            if((firstArray[0] > secondArray[0]) && (firstArray[firstLength - 1] < secondArray[secondLength - 1])) {
-                return nestable = true;
-            } else {
-                return nestable = false;
-            }
 
+        for (int i = 0; i < secondArray.length; i++) {
+            if ((firstArray[0] > secondArray[0]) && (firstArray[firstLength - 1] < secondArray[secondLength-1])){
+        return true;
+            } else {
+                return false;
+            }
         }
-        return nestable;
+        return canNest;
     }
-}
+    }
+
