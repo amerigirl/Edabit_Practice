38b2623336a5503001c8f44c021653fa07d4b5e1
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-09-13 22:30:50 -0500

    reviewed matchesLastItem

--- src/com/company/easy/MatchesLastItem.java
+++ src/com/company/easy/MatchesLastItem.java
@@ -1,23 +1,31 @@
 package com.company.easy;
 
 public class MatchesLastItem {
-    public static boolean isEqualToLastItem(String[] mainString){
+    public static boolean isEqualToLastItem(String[] mainString) {
 
-        /*Create a function that takes an array of items and checks if the last item matches the rest of the array concatenated together. */
+        /*Create a function that takes an array of items and returns true if the last item matches the rest of the array concatenated together. */
 
-            String newString = ""; //instantiates a new empty string to hold the new value
+        /*1. create the function and params
+          2. create a param to hold string for all items
+          3. create a forloop to cycle through mainstring
+          4. create a conditional to check
+        */
 
-            for (int i = 0; i< mainString.length-1; i++){ //cycles through mainString
-                newString += mainString[i];  //place new separate strings in container
-            }
-            if(newString.equals(mainString[mainString.length-1])){
+        String newString = "";
+
+        for (int i = 0; i < mainString.length; i++) {
+            newString += mainString[i];
+        }
+
+        if (newString.equals(mainString[mainString.length - 1])) {
             return true;
         }
-    return false;
+        return false;
     }
-
-
 }
+
+
+
 
 
 
