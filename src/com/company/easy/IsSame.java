2e9c2d7a2bea18899d81d6a66ed05fd17c4b1eb4
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-09-13 22:14:26 -0500

    reviewed IsSame

--- src/com/company/easy/IsSame.java
+++ src/com/company/easy/IsSame.java
@@ -1,12 +1,18 @@
 package com.company.easy;
 
 public class IsSame {
+    //Create a function that returns true if two numbers are the same.
+    /*1. create a method with no parameters
+      2. create a conditional that compares two numbers
+      return
+    */
     public static boolean isSame(int x, int y){
-    if(x == y){
-        return true;
-    } else{
-        return false;
-    }
+     if(x == y){
+         return true;
+     } else {
+         return false;
+     }
+
     }
 
 }
